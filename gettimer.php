c2d2e3750e1b9ae2d164ecbbbd08735c81f2fe1b
Manu Barsainyan <mbarsainyan210692@gmail.com>
2016-12-08 17:47:26 -0800

    Update gettimer.php

--- gettimer.php
+++ gettimer.php
@@ -52,13 +52,42 @@ $min=intval(($time-$hr*3600)/60);
            echo $rows;
 			 printf(' 
 
-                   <div class="content-section today-deal">
+                    <div class="content-section today-deal">
                         <div class="product small-12 columns">
-						
+					
                             <div class="timer">
-                                Ends in: <span class="countdown timeout" data-seconds-left="5400"><span class="hours">%s:%s,%s,%s</span><span class="clearDiv"></span></span>
+                                Ends in: <span class="countdown timeout" data-seconds-left="5400"><span class="hours">%s:</span><span class="minutes">%s:</span><span class="seconds">00</span><span class="clearDiv"></span></span>
                             </div>
-                      
+                            <div class="product-image">
+                                <a href="#">
+                                    <img src="%s" alt="">
+                                    <img src="./assets/product4-2.jpg" alt="" class="animated pulse">
+                                </a>
+
+                                <div class="pro-buttons menu-centered">
+                                    <ul class="menu">
+                                        <li><a href="#" class="addWishList" title="Add to wish list"><i class="fa fa-heart"></i></a></li>
+                                        <li><a href="#" title="Open Product Page"><i class="fa fa-retweet"></i></a></li>
+                                        <li><a href="#" title="Quick View"><i class="fa fa-search-plus"></i></a></li>
+                                        <li><a href="#" class="addCart" title="Add to cart"><i class="fa fa-shopping-cart"></i></a></li>
+                                    </ul>
+                                </div><!-- product buttons /-->
+
+                            </div><!-- Product Image /-->
+                            <div class="product-title">
+                                <a href="http://www.webfulcreations.com/envato/webful_marketplace/html/single-product.html">%s</a>
+                            </div><!-- product title /-->
+                            <div class="product-meta">
+                                <div class="prices">
+                                    <span class="price">$12</span> / Piece
+                                    <span class="off-tag">-25%</span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div><!-- product meta /-->
+                        </div><!-- Product /-->
+
+                        <div class="clearfix"></div>
+                    </div><!-- content section /-->
 
                        ',$hr,$min,$rows[$line['product_id']][3],$rows[$line['product_id']][1]);
          }
