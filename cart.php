ff13ba36c7b024ad97ae2391ded197ba666cc4f0
Ashutosh Singh <ashutosh.singh@sjsu.edu>
2016-12-09 14:14:07 -0800

    Updated img path

--- cart.php
+++ cart.php
@@ -116,7 +116,7 @@
         <div class="a-column a-span2 a-text-left" style="float: right;margin-top: -27px;">
             <div class="a-spacing-small pqty">
                 <input type='number' max="10" min="0" value='' class='qty' />
-                <div class="inc  button incrementer"><img class="plus" src="../marketplace-cmpe272/assets/plus.svg">+</div><div  class="dec  button incrementer"><img class="minus" src="../marketplace-cmpe272/assets/minus.jpg">-</div>
+                <div class="inc  button incrementer"><img class="plus" src="../assets/plus.svg">+</div><div  class="dec  button incrementer"><img class="minus" src="../assets/minus.jpg">-</div>
             </div>
             <p>
             </p>
