f7b23fba6c8cc8faf749cd19a1769f234533898d
Manu Barsainyan <mbarsainyan210692@gmail.com>
2016-12-09 05:57:24 -0800

    Update gettimer.php

--- gettimer.php
+++ gettimer.php
@@ -58,7 +58,7 @@
                         <div class="clearfix"></div>
                     </div><!-- section title /-->
 
-                    <php 
+                    <?php 
                          printf('copied');
                          
                          ?>
