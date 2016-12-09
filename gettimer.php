21182a597ff669bbf99d8e17c821f8865c459a5c
Manu Barsainyan <mbarsainyan210692@gmail.com>
2016-12-09 08:14:01 -0800

    Update gettimer.php

--- gettimer.php
+++ gettimer.php
@@ -74,7 +74,17 @@ else
 echo 'connected';
 $result = pg_query($con, "select * from todaysdeals");
 	
+echo '<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">';
 while($line=pg_fetch_assoc($result))
 {
         
@@ -110,50 +120,29 @@ $min=intval(($time-$hr*3600)/60);
 	   {
 		 $rows=$hiral_rows; 
 	   }
-	echo ' 
-                    <div class="content-section today-deal">
-                        <div class="product small-12 columns">
-					
-                            <div class="timer">
-                                Ends in: <span class="countdown timeout" data-seconds-left="5400"><span class="hours">'.$hr.':</span>
-				<span class="minutes">'.$min.':</span><span class="seconds">'.$sec.'</span><span class="clearDiv"></span></span>
-                            </div>
-                          <div class="product-image">
-                                <a href="#">
-                                    <img src="'.$rows[$line['product_id']][3].'" alt="">
-                                    <img src="./assets/product4-2.jpg" alt="" class="animated pulse">
-                                </a>
-
-                                <div class="pro-buttons menu-centered">
-                                    <ul class="menu">
-                                        <li><a href="#" class="addWishList" title="Add to wish list"><i class="fa fa-heart"></i></a></li>
-                                        <li><a href="#" title="Open Product Page"><i class="fa fa-retweet"></i></a></li>
-                                        <li><a href="#" title="Quick View"><i class="fa fa-search-plus"></i></a></li>
-                                        <li><a href="#" class="addCart" title="Add to cart"><i class="fa fa-shopping-cart"></i></a></li>
-                                    </ul>
-                                </div><!-- product buttons /-->
-
-                            </div><!-- Product Image /-->
-                            <div class="product-title">
-                                <a href="http://www.webfulcreations.com/envato/webful_marketplace/html/single-product.html">
-				'.$rows[$line['product_id']][1].'
-				</a>
-                            </div><!-- product title /-->
-                            <div class="product-meta">
-                                <div class="prices">
-                                    <span class="price">$12</span> / Piece
-                                    <span class="off-tag">-25%</span>
-                                </div>
-                                <div class="clearfix"></div>
-                            </div><!-- product meta /-->
-                        </div><!-- Product /-->
-                        <div class="clearfix"></div>
-                    </div><!-- content section /-->
-                       ';   
+	echo '<div class="item">
+      <img src="'.$rows[$line['product_id']][3].'" alt="Chania">
+      <div class="carousel-caption">
+        <h3>'.$rows[$line['product_id']][1].'</h3>
+        <p>The atmosphere in Chania has a touch of Florence and Venice.</p>
+      </div>
+    </div>';
+    
     
 	
 }
+echo '  </div>
 
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>'
 }
                         
                         ?>
